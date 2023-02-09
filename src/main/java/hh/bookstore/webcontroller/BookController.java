90d05c7879b767e1ce207d746c97eec44be4304c
poddui <jimi8888.podduikin@gmail.com>
2023-02-09 16:10:40 +0200

    Lisäsin add ja delete featuren

--- src/main/java/hh/bookstore/webcontroller/BookController.java
+++ src/main/java/hh/bookstore/webcontroller/BookController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.bookstore.domain.Book;
 import hh.bookstore.domain.BookRepository;
 
 @Controller
@@ -22,4 +26,21 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("books", new Book());
+		return "addbook";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveBook(Book book) {
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
 }
