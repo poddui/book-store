32718716f26907d653d10e882a0c35b7059c6a3e
poddui <jimi8888.podduikin@gmail.com>
2023-03-15 14:48:48 +0200

    lisätty dockerfile

--- src/main/java/hh/bookstore/webcontroller/BookController.java
+++ src/main/java/hh/bookstore/webcontroller/BookController.java
@@ -48,7 +48,7 @@ public class BookController {
 	}
 	
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-	@PreAuthorize("hasRole('ADMIN')")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../booklist";
